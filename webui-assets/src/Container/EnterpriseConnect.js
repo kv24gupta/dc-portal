3742c56573eb59f8292e76f440dcc7a17ec2ee5f
Shubham Paskanti <spaskanti1@gmail.com>
2021-08-11 19:01:03 +0530

    EC Wireframe Changes

--- webui-assets/src/Container/EnterpriseConnect.js
+++ webui-assets/src/Container/EnterpriseConnect.js
@@ -7,12 +7,13 @@ export default class EnterpriseConnect extends React.Component {
 
   componentDidMount() {
     this.highChart();
+    this.props.setPersonaHandler([]);
   }
 
   // istanbul ignore next
   highChart() {
     // Create the chart
-    Highcharts.chart("container", {
+    Highcharts.chart("pieContainer", {
       chart: {
         type: "pie",
       },
@@ -20,9 +21,9 @@ export default class EnterpriseConnect extends React.Component {
         text: "Quality of Service",
         style: {
           color: "#005EB8",
-          fontWeight: "bolder",
+          fontWeight: "bold",
           fontFamily: "GE Inspira Sans !important",
-          fontSize: "1.5vw !important",
+          fontSize: "1.2vw !important",
         },
       },
       subtitle: {
@@ -36,6 +37,9 @@ export default class EnterpriseConnect extends React.Component {
         x: 0,
         y: 0,
         useHTML: true,
+      },
+      credits: {
+        enabled: false,
       },
       plotOptions: {
         pie: {
@@ -56,7 +60,7 @@ export default class EnterpriseConnect extends React.Component {
         },
       },
       tooltip: {
-        valueSuffix: "%",
+        valueSuffix: " %",
       },
       series: [
         {
@@ -76,14 +80,162 @@ export default class EnterpriseConnect extends React.Component {
         },
       ],
     });
+
+    Highcharts.chart("barContainer", {
+      chart: {
+        type: "bar",
+      },
+      title: {
+        text: "Agent Health Status Report",
+        style: {
+          color: "#005EB8",
+          fontWeight: "bold",
+          fontFamily: "GE Inspira Sans !important",
+          fontSize: "1.2vw !important",
+        },
+      },
+      /* subtitle: {
+        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+      }, */
+      xAxis: {
+        categories: ["Ex1", "Ex2"],
+        title: {
+          text: null,
+        },
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Agent",
+          align: "high",
+        },
+        labels: {
+          overflow: "justify",
+        },
+      },
+      tooltip: {
+        valueSuffix: " %",
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: false,
+          },
+          pointPadding: 0,
+          borderWidth: 0,
+        },
+      },
+      /* legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: 0,
+        y: 30,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor:
+          Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+        shadow: true
+      }, */
+      credits: {
+        enabled: false,
+      },
+      series: [
+        {
+          name: "bar1",
+          data: [20, 30],
+        },
+        {
+          name: "bar2",
+          data: [30, 40],
+        },
+      ],
+    });
+
+    Highcharts.chart("columnContainer", {
+      chart: {
+        type: "column",
+      },
+      title: {
+        text: "Event Reporting",
+        style: {
+          color: "#005EB8",
+          fontWeight: "bold",
+          fontFamily: "GE Inspira Sans !important",
+          fontSize: "1.2vw !important",
+        },
+      },
+      /* subtitle: {
+        text: 'Source: WorldClimate.com'
+      }, */
+      xAxis: {
+        categories: ["Ex1", "Ex2"],
+        crosshair: true,
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Events",
+        },
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat:
+          '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+        footerFormat: "</table>",
+        shared: true,
+        useHTML: true,
+      },
+      credits: {
+        enabled: false,
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0,
+          borderWidth: 0,
+        },
+      },
+      series: [
+        {
+          name: "bar1",
+          data: [20, 30],
+        },
+        {
+          name: "bar2",
+          data: [45, 20],
+        },
+      ],
+    });
   }
 
   render() {
     return (
       <Fragment>
-        <div className="container-lg w-100 p-3 borderStyle mb-5">
+        <div className="container-lg w-100 p-3 borderStyle mb-3">
           <div className="row mx-1">
-            <div className="col service-tile-content m-1 borderStyle p-2 text-left">
+            <div className="col mx-1 borderStyle">
+              <div className="card border-0">
+                <div className="card-body p-0">
+                  <div id="barContainer" style={{ height: "15rem" }}></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mx-1 borderStyle">
+              <div className="card border-0">
+                <div className="card-body p-0">
+                  <div id="columnContainer" style={{ height: "15rem" }}></div>
+                </div>
+              </div>
+            </div>
+            <div className="col mx-1 borderStyle">
+              <div className="card border-0">
+                <div className="card-body p-0">
+                  <div id="pieContainer" style={{ height: "15rem" }}></div>
+                </div>
+              </div>
+            </div>
+            {/* <div className="col service-tile-content m-1 borderStyle p-2 text-left">
               <div className="titles">SUMMARY</div>
               <div className="row service-details">
                 <div className="col service-tile-content">Gateway Count: 8</div>
@@ -95,8 +247,8 @@ export default class EnterpriseConnect extends React.Component {
                   Agent Status: <span className="redDot"></span>
                 </div>
               </div>
-            </div>
-            <div className="col service-tile-content m-1 borderStyle p-2 text-left">
+            </div> */}
+            {/* <div className="col service-tile-content m-1 borderStyle p-2 text-left">
               <div className="titles">AGENTS - CLIENTS & SERVER</div>
               <div className="row service-details g-2">
                 <div className="col service-tile-content">
@@ -112,17 +264,21 @@ export default class EnterpriseConnect extends React.Component {
                   SCX Racer DB Server Agent #959034
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="container-lg w-100 p-3 borderStyle">
-          <div className="row service-text">
-            <div className="col-6 service-tile-content mt-2">
-              <div className="titles">SUBSCRIPTION SUMMARY</div>
-              <ul className="pl-3 text-success">
-                <li>Gateway #GAT93027387(CORP POC): Healthy</li>
+          <div className="row service-text p-3">
+            <div className="col service-tile-content borderStyle p-3 mx-1">
+              <div className="titles mb-1">SUMMARY</div>
+              <ul className="list-unstyled service-details m-0">
+                <li>Licenses Has Been Created: 6</li>
+                <li>Active Client/Server Instance: 10</li>
+                <li>Active Connectivity in Client Pool: 30</li>
+                <li>Gateway Scaling: 5</li>
+                <li>Failed Connection Since Launched: 5</li>
               </ul>
-              <div className="row service-text">
+              {/* <div className="row service-text">
                 <div className="col service-tile-content">
                   <div className="titles">ALERTS</div>
                   <ul className="list-unstyled service-details">
@@ -131,14 +287,34 @@ export default class EnterpriseConnect extends React.Component {
                     </li>
                   </ul>
                 </div>
-              </div>
+              </div> */}
             </div>
-            <div className="col-6">
-              <div className="card border-0">
-                <div className="card-body p-0">
-                  <div id="container" style={{ height: "15rem" }}></div>
+            <div className="col service-tile-content borderStyle p-3 mx-1">
+              <div className="titles mb-1">Additional Links</div>
+              <ul className="list-unstyled service-details m-0">
+                <li>
+                  <a href="#">@Access Customer Portal</a>
+                </li>
+                <li>
+                  <a href="#">@Webhook Creations</a>
+                </li>
+                <li>
+                  <a href="#">@New Gateway Service Request</a>
+                </li>
+                <li>
+                  <a href="#">@New License Request</a>
+                </li>
+              </ul>
+              {/* <div className="row service-text">
+                <div className="col service-tile-content">
+                  <div className="titles">ALERTS</div>
+                  <ul className="list-unstyled service-details">
+                    <li>
+                      Client Agent #9281824 (Customer Connect JMS) is restarting
+                    </li>
+                  </ul>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
