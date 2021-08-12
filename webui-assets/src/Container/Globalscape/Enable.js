76c357f6055703cc9c79388674f1d2de0f49328b
kv24gupta <88764623+kv24gupta@users.noreply.github.com>
2021-08-12 18:01:59 +0530

    Update Enable.js

--- webui-assets/src/Container/Globalscape/Enable.js
+++ webui-assets/src/Container/Globalscape/Enable.js
@@ -9,32 +9,28 @@ export default class Enable extends React.Component {
   }
 
   render() {
-    return (  
-        <Modal show={this.props.openModal} onHide={this.props.closeModal}>
-          <Modal.Header closeBut ton>
-            <Modal.Title>Enable</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p className="form-group row">
-              <label className="col-sm-4 col-form-label" htmlFor="gs_eve-name">Event Name*</label>
-              <div className="col-sm-8"><i className="fa fa-user icon"></i>
-                <input className="form-control form-control-sm" type="text" id="gs_eve-name" name="gs_eve-name" />    </div>
-            </p>
-            <p className="form-group row">
-              <label className="col-sm-4 col-form-label" htmlFor="gs_eve-sso">New SSO ID</label>
-              <div className="col-sm-8"><i className="fa fa-user icon"></i>
-                <input className="form-control form-control-sm" type="text" id="gs_eve-sso" name="gs_eve-sso" /></div>
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.props.closeModal}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.props.closeModal}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+    return (
+      <Modal show={this.props.openModal} onHide={this.props.closeModal}>
+        <Modal.Header closeBut ton>
+          <Modal.Title>Enable</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="form-group row">
+            <label className="col-sm-4 col-form-label" htmlFor="gs_eve-name">Event Name*</label>
+            <div className="col-sm-8"><i className="fa fa-user icon"></i>
+              <input className="form-control form-control-sm" type="text" id="gs_eve-name" name="gs_eve-name" value={this.props.eventName} />  
+            </div>
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.closeModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.props.closeModal}>
+            Enable
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
